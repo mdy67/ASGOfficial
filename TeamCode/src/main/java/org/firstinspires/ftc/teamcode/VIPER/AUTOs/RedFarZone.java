d5de0f7f4a7b953ba1304cd038f43438ea27c8d9
cader <caderinvader@outlook.com>
2026-05-01 11:36:17 -0400

    good goon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VIPER/AUTOs/RedFarZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VIPER/AUTOs/RedFarZone.java
@@ -165,7 +165,7 @@ public class RedFarZone extends LinearOpMode {
                             } else if (goonCounter == 2) {
                                 state = State.CORNER_INTAKES;
                             } else if (goonCounter == 3) {
-                                state = State.CORNER_INTAKES;
+                                state = State.CORNER_INTAKES_2;
                             } else if (goonCounter == 4) {
                                 state = State.CORNER_INTAKES_2;
                             } else if (goonCounter == 5) {
